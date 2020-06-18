93ffcfe9143e20eaa20f30e1e445de3ab88f502f
Camelo <lucascamelo62@gmail.com>
2020-06-18 00:12:22 -0300

    Terceiro commit

---
+++ atribuicaoVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Atribuição de Variáveis");
+
+const nome = "Lucas";
+const sobrenome = "Camelo"
+
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+
+const nomeNovo = nome, sobrenome;
+
+console.log(nomeNovo);
---
+++ operacaoesAritimeticas.js
@@ -0,0 +1,3 @@
+console.log("Operações Aritimeticas");
+
+console.log(parseInt("2") + parseInt("2"));
